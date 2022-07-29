dd84350769524809ef19c2ac30beb6b3167c6918
Datta Manik Shinde <74047423+dattashindea99@users.noreply.github.com>
2022-07-29 13:11:34 +0530

    first

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductsService } from '../products.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+  cart:any[]=[];
+  total=0;
+
+
+  constructor(private service: ProductsService, private http: HttpClient, private router: Router) { }
+
+  ngOnInit(): void {
+    this.service.cartitems().subscribe((Res:any) => {
+      console.log(Res)
+      this.cart=Res;
+
+      for(var i=0;i<this.cart.length;i++){
+          // this.total=this.total+parseInt(this.cart[i]['price']);
+          this.total+=parseInt(this.cart[i]['price']);
+
+      }
+    })
+  }
+
+  deletecartitems(cartitemId: any) {
+    this.service.deleteCartItem(cartitemId).subscribe((response:any)=>{
+        this.service.cartitems().subscribe((Res:any) => {
+          console.log(Res)
+          this.total=0;
+          this.cart=Res;
+    
+          for(var i=0;i<this.cart.length;i++){
+              // this.total=this.total+parseInt(this.cart[i]['price']);
+              this.total+=parseInt(this.cart[i]['price']);
+          }
+        })
+
+      
+    })
+
+
+
+  }
+
+}
---
+++ src/app/products.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  private productslist = "http://localhost:8000";
+
+  constructor(private http: HttpClient) { }
+
+  getallproducts() {
+    let resp: any = this.http.get(this.productslist+"/laptops");
+    // resp.subscribe((data: any) => this.productslist = data);
+    // resp.subscribe((data: any) => console.log(data))
+    return resp;
+  }
+
+  addtocart(product: any){
+    let resp=this.http.post(this.productslist+"/cart/",product)
+    return resp;
+  }
+
+  cartitems(){
+    let resp: any = this.http.get(this.productslist+"/cart");
+    return resp;
+  }
+
+  deleteCartItem(id:any){
+    let resp:any=this.http.delete(this.productslist+"/cart/"+id);
+    return resp;
+  }
+}
